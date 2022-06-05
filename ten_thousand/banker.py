0db30bf13253e73bacc96ef706149e8b5fe10050
Anthony Turinski <71283758+Terransky@users.noreply.github.com>
2022-06-04 19:36:52 -0700

    cleared all banker tests

--- ten_thousand/banker.py
+++ ten_thousand/banker.py
@@ -1,16 +1,30 @@
 
+# from game_logic import GameLogic
+
 class Banker:
     """docstring"""
 
-    def __init__(self):
-        pass
+    def __init__(self, balance=0, shelved=0):
+        # balance is total, shelf is temporary round score
+        self.balance = balance
+        self.shelved = shelved
 
-    def shelf(self):  # input: amount of points(integer) to add to shelf
-        pass  # output: no return, temporarily stores unbanked points
+
+
+    def shelf(self, shelf_points):  # input: amount of points(integer) to add to shelf
+        self.shelved = shelf_points
+        # output: no return, temporarily stores unbanked points
 
     def bank(self):  # takes shelf points and adds to total bank, resets shelf to 0
-        pass  # output: sum of bank and shelf
+        self.balance += self.shelved
+        self.shelved = 0
+        return self.balance
+        # output: return sum of bank and shelf
 
     def clear_shelf(self):  # clear shelf points
-        pass
+        self.shelved = 0
 
+
+
+if __name__ == "__main__":
+    pass
