7fde0be6daa4f24ac65dec37fefd0fbb4837f460
Jae Loney <jaimieloney@gmail.com>
2022-06-11 18:10:39 -0400

    Running into issues with implementing the zilch method. THIS BRANCH DOES NOT HAVE THE ZILCH METHOD WORKING

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -19,6 +19,7 @@ class Game:
         self.game_logic = GameLogic()
         self.banker = Banker()
         self.nums = ""
+        self.current_score = 0
 
     def play(self, roller=None):
         """
@@ -57,44 +58,78 @@ class Game:
         elif usr_input == "n" or usr_input == "no":
             print( "OK. Maybe another time")
 
-    def rolled_dice(self, roller):
-        """
-        This method displays the rolled dice.
-        """
-        self.nums = self.rolling_dice(roller)
-        print(f"Rolling {self.die} dice...\n*** {self.nums} ***\nEnter dice to keep, or (q)uit:")
-
-    def shelf(self, usr_input):
-        """
-        This method keeps track of the dice and scores in the shelf.
-        """
-        nums_tuple = tuple([int(num) for num in self.nums.split()])
-        keep_nums = [int(num) for num in usr_input]
-        sanitized_keep_nums = [x for x in keep_nums if x in nums_tuple]
-        self.banker.shelf(self.game_logic.calculate_score(sanitized_keep_nums))
-        self.die -= len(sanitized_keep_nums)
-        print(f"You have {self.banker.shelved} unbanked points and {self.die} dice remaining\n(r)oll again, (b)ank your points or (q)uit:")
-
-    def bank(self):
-        """
-        This method banks the score, clears the shelf and ends the current round.
-        """
-        print(f"You banked {self.banker.shelved} points in round {self.round}")
-        self.banker.bank()
-        print(f"Total score is {self.banker.balance} points")
-        self.round += 1
-        self.die = 6
-
+    
     def rolling_dice(self, roller)  -> str:
+        # This arrow function is saying our *return* value will be a data-type check. If we are returning something that is not a string, the program will catch it before it runs. 
         """
-        This method rolls the dice.
+        This method rolls the dice and returns the actual numbers on the dice as a string.
         """
         if roller:
             int_list_of_die = roller(self.die)
         else:
             int_list_of_die = self.game_logic.roll_dice(self.die)
         return ' '.join([str(number) for number in int_list_of_die])
+
+
+    def rolled_dice(self, roller):
+        """
+        This method displays the rolled dice. We are using this method to calculate the score of the returned dice and determine whether the user has zilch points or whether they can choose to shelve points from certain dice.
+        """
+        self.nums = self.rolling_dice(roller)
+        self.nums = self.nums.split(" ")
+        print(self.nums)
+        
+
+        print(f"Rolling {self.die} dice...\n*** {self.nums} ***")
+        if self.current_score==0:
+            self.zilch()
+            
+        else:
+            print("Enter dice to keep, or (q)uit:")
+            
+
+    def shelf(self, usr_input):
+        """
+        This method keeps track of the dice and scores in the shelf. This is where the user chooses dice to shelve
+        """
+        nums_tuple = tuple([int(num) for num in self.nums.split()])
+        keep_nums = [int(num) for num in usr_input]
+        sanitized_keep_nums = [x for x in keep_nums if x in nums_tuple]
+        self.current_score = self.game_logic.calculate_score(sanitized_keep_nums)
+        self.banker.shelf(self.current_score)
+        self.die -= len(sanitized_keep_nums)
+        print(f"You have {self.banker.shelved} unbanked points and {self.die} dice remaining\n(r)oll again, (b)ank your points or (q)uit:")
+
+    def bank(self):
+        """
+        This method banks the score, clears the shelf and ends the current round. This is the function we are saying will create a new round and a new round score of 0 (clearing the shelf).
+        """
+        if self.current_score == 0:
+            print(f"You banked 0 points in round {self.round}")        
+
+        else:
+            print(f"You banked {self.banker.shelved} points in round {self.round}")
+        
+        self.banker.bank()
+        # This takes shelf points and adds to total bank, resets shelf to 0
+        print(f"Total score is {self.banker.balance} points")
+
+        self.round += 1
+        self.die = 6
+        #What this is doing here with the round and the die is starting a new round. 
+
     
+    def zilch(self):
+        """This method returns a custom comment if the dice roll does not include any scoring die."""
+        # self.current_score()
+        
+        self.bank()        
+        self.banker.clear_shelf()
+        # if self.current_score == 0:
+            
+        print("****************************************\n**        Zilch!!! Round over         **\n****************************************")
+
+
     def roll_again(self, roller, usr_input):
         """
         This method allows the user to roll the dice again in the current round of game.
@@ -114,6 +149,7 @@ class Game:
                 print("You're out of die")
                 self.bank()
                 break   
+
 
     def end_game(self):
         sys.exit(f"\nThanks for playing. You earned {self.banker.balance} points")
